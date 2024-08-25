28baaf0c41c4565327bf3dd6b0c40858461247aa
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-08-25 15:15:53 +0500

    Setup Tools

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Kidney-Tumor-Classification"
+AUTHOR_USER_NAME = "saadan1234"
+SRC_REPO = "Kidney-Tumor-Classification"
+AUTHOR_EMAIL = "msaadan.bese21seecs@seecs.edu.pk"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author = AUTHOR_USER_NAME,
+    author_email= AUTHOR_EMAIL,
+    description="Kidney Tumor Classifier",
+    long_description=long_description,
+    long_description_content = "text/markdown",
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    
+
+)
