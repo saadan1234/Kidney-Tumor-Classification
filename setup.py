eabaf9d7311e4dd52ab0104a5d15bead2a11c200
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-08-25 15:41:08 +0500

    Requirements.txt

--- setup.py
+++ setup.py
@@ -19,6 +19,10 @@ setuptools.setup(
     long_description=long_description,
     long_description_content = "text/markdown",
     url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
 
 )
