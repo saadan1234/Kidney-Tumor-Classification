cdc8c416b453faa9499f8c880c975a5178a86dcb
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-08-27 23:14:59 +0500

    Modularity

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.kidneyClassifier import logger 
+from kidneyClassifier import logger
+from kidneyClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logging.")
+STAGE_NAME = "Data Ingestion Stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
