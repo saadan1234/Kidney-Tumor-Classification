c9466bb73a4e4d034897c587c0efeb3d741d12fd
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-08-28 22:41:31 +0500

    Model Training Pipeline

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from kidneyClassifier import logger
 from kidneyClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from kidneyClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
+from kidneyClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
+# STAGE_NAME = "Data Ingestion Stage"
+
+# try:
+#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#     obj = DataIngestionTrainingPipeline()
+#     obj.main()
+#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+# STAGE_NAME = "Preparing Base Model Stage"
+
+# try:
+#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#     obj = PrepareBaseModelPipeline()
+#     obj.main()
+#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+STAGE_NAME = "Model Training Stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
-except Exception as e:
-    logger.exception(e)
-    raise e
-
-STAGE_NAME = "Preparing Base Model Stage"
-
-try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = PrepareBaseModelPipeline()
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
 except Exception as e:
---
+++ src/kidneyClassifier/pipeline/stage_03_model_training.py
@@ -0,0 +1,30 @@
+from kidneyClassifier.components.model_training import Training
+from kidneyClassifier.config.configuration import ConfigurationManager
+from kidneyClassifier import logger
+
+STAGE_NAME = "Model Training Stage"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            training_config = config.get_training_config()
+            training = Training(config=training_config)
+            training.get_base_model()
+            training.train_valid_generator()
+            training.train()
+        except Exception as e:
+            raise e
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
