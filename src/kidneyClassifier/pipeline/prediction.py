3f5f361c4e9188c39f5164575cb78621393ede9c
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-08-30 23:51:43 +0500

    App Interface

--- src/kidneyClassifier/pipeline/prediction.py
+++ src/kidneyClassifier/pipeline/prediction.py
@@ -2,16 +2,15 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
+from pathlib import Path
 
 class PredictionPipeline:
     def __init__(self,filename):
         self.filename =filename
 
-
-    
     def predict(self):
         # load model
-        model = load_model(os.path.join("model", "model.h5"))
+        model = load_model(Path("artifacts/training/model.h5"))
 
         imagename = self.filename
         test_image = image.load_img(imagename, target_size = (224,224))
